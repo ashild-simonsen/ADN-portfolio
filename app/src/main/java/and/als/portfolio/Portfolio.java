5d38d1f7cdf16a558ff79fde4187a23f8a2be2e5
Åshild Lobben Simonsen <ashild.simonsen@gmail.com>
2015-10-20 11:15:35 +0200

    onClick event moved from java to xml

--- app/src/main/java/and/als/portfolio/Portfolio.java
+++ app/src/main/java/and/als/portfolio/Portfolio.java
@@ -11,62 +11,36 @@ import android.widget.Toast;
 
 public class Portfolio extends Activity {
 
-    Button btnBuildItBigger;
-    Button btnCapstone;
-    Button btnFotballScoresApp;
-    Button btnLibraryApp;
-    Button btnSpotifyStreamer;
-    Button btnXYZReader;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portfolio);
+    }
 
-        btnBuildItBigger = (Button)findViewById(R.id.btn_build_it_bigger);
-        btnCapstone = (Button)findViewById(R.id.btn_capstone);
-        btnFotballScoresApp = (Button)findViewById(R.id.btn_fotball_scores);
-        btnLibraryApp = (Button)findViewById(R.id.btn_library);
-        btnSpotifyStreamer = (Button)findViewById(R.id.btn_spotify_streamer);
-        btnXYZReader = (Button)findViewById(R.id.btn_xyz_reader);
+    public void showToast(View v) {
+        Context context = getApplicationContext();
+        String text = context.getResources().getString(R.string.toast_launch_project_buttons);
 
-        View.OnClickListener listener = new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_build_it_bigger:      text = String.format(text, context.getString(R.string.btn_build_it_bigger));
+                break;
+            case R.id.btn_capstone:             text = String.format(text, context.getString(R.string.btn_capstone));
+                break;
+            case R.id.btn_fotball_scores:       text = String.format(text, context.getString(R.string.btn_fotball_scores));
+                break;
+            case R.id.btn_library:              text = String.format(text, context.getString(R.string.btn_library));
+                break;
+            case R.id.btn_spotify_streamer:     text = String.format(text, context.getString(R.string.btn_spotify_streamer));
+                break;
+            case R.id.btn_xyz_reader:           text = String.format(text, context.getString(R.string.btn_xyz_reader));
+                break;
+            default: text = "";
+        }
 
-                Context context = getApplicationContext();
-                String text = context.getResources().getString(R.string.toast_launch_project_buttons);
+        int duration = Toast.LENGTH_SHORT;
 
-                switch (v.getId()){
-                    case R.id.btn_build_it_bigger:      text = String.format(text, context.getString(R.string.btn_build_it_bigger));
-                                                        break;
-                    case R.id.btn_capstone:             text = String.format(text, context.getString(R.string.btn_capstone));
-                                                        break;
-                    case R.id.btn_fotball_scores:       text = String.format(text, context.getString(R.string.btn_fotball_scores));
-                                                        break;
-                    case R.id.btn_library:              text = String.format(text, context.getString(R.string.btn_library));
-                                                        break;
-                    case R.id.btn_spotify_streamer:     text = String.format(text, context.getString(R.string.btn_spotify_streamer));
-                                                        break;
-                    case R.id.btn_xyz_reader:           text = String.format(text, context.getString(R.string.btn_xyz_reader));
-                                                        break;
-                    default: text = "";
-                }
-
-                int duration = Toast.LENGTH_SHORT;
-
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-            }
-        };
-
-        btnBuildItBigger.setOnClickListener(listener);
-        btnCapstone.setOnClickListener(listener);
-        btnFotballScoresApp.setOnClickListener(listener);
-        btnLibraryApp.setOnClickListener(listener);
-        btnSpotifyStreamer.setOnClickListener(listener);
-        btnXYZReader.setOnClickListener(listener);
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     @Override
